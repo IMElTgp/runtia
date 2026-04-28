888b571f7f4dbcdc998506ca501638433a4682b2
IMElTgp <2827228085@qq.com>
2026-04-28 22:21:32 +0800

    implemented signal-to-finding skeleton code

---
+++ internal/report/findings.go
@@ -0,0 +1,28 @@
+package report
+
+import "github.com/IMElTgp/container-runtime-analysis/internal/model"
+
+// findings.go converts signals to findings
+// currently, composition is just a TODO
+// just turn model.Signal into model.Finding
+
+// if we take composition into consider, use a hash map to store different kinds of signals
+// for hard-coded signal pairs, generate a composited signal and mark both of the signals in that pair as "covered"
+// "covered" signals may still involve composition, but are not going to be turned into findings
+
+func GenerateFindings(signals []*model.Signal) (findings []*model.Finding) {
+	for _, sig := range signals {
+		findings = append(findings, &model.Finding{
+			Category:        sig.Category,
+			Recommendation:  sig.Recommendation,
+			RelativeThreads: sig.RelativeThreads,
+			RelativeNS:      sig.RelativeNS,
+			RiskLevel:       sig.RiskLevel,
+			Summary:         sig.Summary,
+			Title:           sig.Title,
+			Evidence:        sig.Evidence,
+			MountPoint:      sig.MountPoint,
+		})
+	}
+	return
+}
